344f2b0e966123caed25b3044297d21a59462b7e
Bridget Hartin Pasco <isaacpasco@icloud.com>
2024-05-08 19:31:14 -0700

    deleted everything

--- game/init_script.cpp
+++ game/init_script.cpp
@@ -1,12 +1,6 @@
 #include <raylib.h>
 #include "../engine/runtime.h"
-#include "player.h"
-#include "box.h"
+
 void initscript(Runtime *runtime){
-    Player *bert = new Player;    
-    runtime->register_entity(bert);
-    for(int i =0; i<5000; i++){
-        Box * b = new Box;
-        runtime->register_entity(b);
-    }
+
 }
